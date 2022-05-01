f6898189d06b94cf980b2c9c00710dcda360ffca
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2022-05-01 00:04:44 -0800

    Add JUnit test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,6 @@
 public class SkillDemo{
-
+    public static int multiply(int x, int y){
+        return x*y;
+    }
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void test1(){
+        assertEquals(4, SkillDemo.multiply(2, 3));
+    }
+}
